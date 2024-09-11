7f13a36bc767d93751e852218b7633138db039d7
Fritz Lin <uxfritz@163.com>
2024-09-11 10:26:13 +0800

    ln -s but gitignore .npmrc

--- repo_apply.sh
+++ repo_apply.sh
@@ -1,6 +1,6 @@
 echo 'Warning: Remember to back-up as you need!!'
 
-for f in {'.vscode/settings.json',.bash_profile,.bashrc,.bashrc_include,.bashrc_custom,.bashrc_private_post,.bashrc_private_pre,.editorconfig,.gitignore_global,.zshrc}; do
+for f in {'.vscode/settings.json',.bash_profile,.bashrc,.bashrc_include,.bashrc_custom,.bashrc_private_post,.bashrc_private_pre,.editorconfig,.gitignore_global,.zshrc,.npmrc}; do
   [ -r $f ] && [ -e $f ] && ln -s "$FRITXBASH_PATH/$f" "$HOME/$f"
 done
 unset f
